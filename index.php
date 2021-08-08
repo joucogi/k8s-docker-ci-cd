f795e77f19e9f256aaec6d776472d22ff6612eb5
Joel Coll <joel.coll@gmail.com>
2021-08-08 12:11:31 +0200

    Last deployment with tags

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "Thi is the last deployment with tags to test Example DockerHub and GitHub integration with Deployment. by Joucogi";
+echo "This is the last deployment with tags to test Example DockerHub and GitHub integration with Deployment. by Joucogi";
