08abcff40088db83ff6bca29b2bd0de368c6218b
Joel Coll <joel.coll@gmail.com>
2021-10-26 13:31:16 +0200

    Update text

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo "This is the last deployment with tags to test Example DockerHub and GitHub integration with Deployment. by Joucogi";
+echo "First change, This is the last deployment with tags to test Example DockerHub and GitHub integration with Deployment. by Joucogi";
